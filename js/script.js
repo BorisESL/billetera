7c47b4a52424415d01aadc66752f6d357d0eefb9
boris <boris.salgadolorca@gmail.com>
2024-02-28 09:02:57 -0300

    credenciales

--- js/script.js
+++ js/script.js
@@ -1,46 +1,25 @@
 $(document).ready(function() {
+    // Datos de usuario para la autenticación
+    var usuarioValido = {
+      correo: 'boris@gmail.com',
+      contraseña: '12345'
+    };
+  
     $('#loginForm').submit(function(event) {
-    event.preventDefault();
-    var username = $('#username').val();
-    var password = $('#password').val();
+      event.preventDefault();
+  
+      // Obtener los valores de correo y contraseña del formulario
+      var correo = $('#email').val();
+      var contraseña = $('#password').val();
+  
       // Verificar las credenciales
-    if (username === 'admin' && password === '12345') {
-        // Credenciales válidas, redirigir a la pantalla de wallet
-        window.location.href = 'wallet.html';
-    } else {
+      if (correo === usuarioValido.correo && contraseña === usuarioValido.contraseña) {
+        // Credenciales válidas, redirigir a la página menú principal
+        window.location.href = '../pages/menup.html';
+      } else {
         // Credenciales inválidas, mostrar mensaje de error
-        alert('Usuario o contraseña invalido. Inténtalo de nuevo.');
-    }
+        alert('Usuario o contraseña incorrectos. Inténtalo de nuevo.');
+      }
     });
-    });
-    $(document).ready(function() {
-    var balance = 0;
-    function updateBalance() {
-    $('#balance').text(balance.toFixed(2));
-    }
-    $('#depositBtn').click(function() {
-    var amount = parseFloat($('#amount').val());
-    if (!isNaN(amount) && amount > 0) {
-        balance += amount;
-        updateBalance();
-        $('#amount').val('');
-        alert('Deposit realizado!');
-    } else {
-        alert('Monto invalido. Por favor ingrese un número positivo.');
-    }
-    });
-    $('#withdrawBtn').click(function() {
-    var amount = parseFloat($('#amount').val());
-    if (!isNaN(amount) && amount > 0 && amount <= balance) {
-        balance -= amount;
-        updateBalance();
-        $('#amount').val('');
-        alert('Retiro exitoso!');
-    } else {
-        alert('Cantidad no válida. Ingrese un número válido dentro de su saldo.');
-    }
-    });
-    });
-
-    
-    
+  });
+  
